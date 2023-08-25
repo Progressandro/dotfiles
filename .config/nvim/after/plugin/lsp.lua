490418df9cebc3d27e2fde1811753a75719e4736
Alejandro Fabrega <progressandro@gmail.com>
2023-08-25 13:15:51 -0300

    Restructured

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,23 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.ensure_installed({
+	'tsserver',
+	'eslint',
+})
+
+lsp.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({ buffer = bufnr })
+end)
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lsp.setup()
+
+local cmp = require('cmp')
+cmp.setup({
+	mapping = {
+		['<Tab>'] = cmp.mapping.confirm({ select = false }),
+		['<C-\\>'] = cmp.mapping.complete(),
+	}
+})
