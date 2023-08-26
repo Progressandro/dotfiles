1193af6a024c21f58ad83d539d3af5723a24ed98
Alejandro F <progressandro@gmail.com>
2023-08-26 03:27:29 -0300

    Added tmux navigator remap for netrw

--- .config/nvim/lua/progressandro/remap.lua
+++ .config/nvim/lua/progressandro/remap.lua
@@ -1,2 +1,22 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<C-l>", ":<C-U>TmuxNavigateRight<CR>", { silent = true, buffer = 0 })
+
+
+local function netrw_mapping()
+    local bufmap = function(lhs, rhs)
+        local opts = { buffer = true, remap = true }
+        vim.keymap.set('n', lhs, rhs, opts)
+    end
+
+    -- close window
+    bufmap('<C-l>', ':<C-U>TmuxNavigateLeft<cr>')
+end
+
+local user_cmds = vim.api.nvim_create_augroup('user_cmds', { clear = true })
+vim.api.nvim_create_autocmd('filetype', {
+    pattern = 'netrw',
+    group = user_cmds,
+    desc = 'Keybindings for netrw',
+    callback = netrw_mapping
+})
