b44631e7c2d7e1b300f6d3d537e7ab6192d03502
Alejandro F <progressandro@gmail.com>
2023-08-30 19:26:18 -0300

    updates

--- .config/nvim/lua/progressandro/remap.lua
+++ .config/nvim/lua/progressandro/remap.lua
@@ -1,9 +1,10 @@
+vim.g.mapleader = " "
 -- Navigation
 vim.keymap.set("i", "jj", "<Esc>")
 -- Move current line / block of text in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 -- Center on cursor
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
