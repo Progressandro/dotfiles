74a8857b6339b61a016b50c82d7126aa44837b6c
Alejandro F <progressandro@gmail.com>
2023-08-31 21:20:12 -0300

    Catpuccin for tmux

--- .config/nvim/lua/progressandro/packer.lua
+++ .config/nvim/lua/progressandro/packer.lua
@@ -72,4 +72,5 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use('vimpostor/vim-tpipeline')
 end)
