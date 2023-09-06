03449c9aed4d161c1ca62d2bde95305afd60b7aa
Alejandro F <progressandro@gmail.com>
2023-09-06 16:53:04 -0300

    Added packer and treesitter

--- .config/nvim/lua/progressandro/packer.lua
+++ .config/nvim/lua/progressandro/packer.lua
@@ -5,5 +5,8 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+  }
 end)
