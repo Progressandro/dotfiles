7a40b69d7d3efddd4d5cfda8170da6cbac78071a
Alejandro F <progressandro@gmail.com>
2023-09-07 12:32:32 -0300

    Added lualine

--- .config/nvim/lua/progressandro/packer.lua
+++ .config/nvim/lua/progressandro/packer.lua
@@ -14,4 +14,10 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use 'christoomey/vim-tmux-navigator'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {
+      'nvim-tree/nvim-web-devicons', opt = true 
+    }
+  }
 end)
