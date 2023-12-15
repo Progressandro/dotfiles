d16d78def0db43231f03c49b9a69a71bfe60cbf9
Alejandro F <progressandro@gmail.com>
2023-12-15 10:34:17 -0300

    Treesitter incremental selection

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,9 @@
 require("nvim-treesitter.configs").setup({
+  build = ':TSUpdate',
+  events = {
+    'BufRead',
+    'BufNewFile',
+  },
 	autotag = {
 		enable = true,
 	},
@@ -24,4 +29,13 @@ require("nvim-treesitter.configs").setup({
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection: '<C-Space>',
+      node_incremental: '<C-Space>',
+      scope_incremental: false,
+      node_decremental: '<bs>',
+    }
+  }
 })
