19c8730c242024e2412ddb3886767b2ec8110ed5
progressandro <progressandro@Alejandros-MacBook-Pro.local>
2024-07-24 19:10:35 -0300

    Update

--- .config/nvim/after/plugin/theme.lua
+++
@@ -1,9 +0,0 @@
-require("catppuccin").setup({
-	transparent_background = true,
-})
-
-color = colors or "catppuccin"
-vim.cmd.colorscheme(color)
-
-vim.api.nvim_set_hl(0, "normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "normalFloat", { bg = "none" })
