9605bdb7f4f177442a13589fae8ba1cb34f71ac3
Progressandro <progressandro@gmail.com>
2025-03-26 22:55:37 -0300

    Trouble and telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,22 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      "nvim-telescope/telescope-frecency.nvim",
+      -- install the latest stable version
+      version = "*",
+    }
+  },
+  config = function()
+    local builtin = require('telescope.builtin')
+    require("telescope").load_extension "frecency"
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fr', ':Telescope frecency workspace=CWD<CR>', { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+  end,
+}
