52022185fde07782f4c6c8150946f4b03d864a6c
Alejandro F <progressandro@gmail.com>
2025-03-27 18:28:18 -0300

    More fixes

--- .config/nvim/lsp/typescript.lua
+++ .config/nvim/lsp/typescript.lua
@@ -1,14 +1,18 @@
 local mason_dir = vim.fn.stdpath("data") .. "/mason/bin/"
+local util = require("config.util")
 return {
-  init_options = { hostInfo = 'neovim' },
-  cmd = { mason_dir .. 'typescript-language-server', '--stdio' },
-  filetypes = {
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx',
-  },
-  root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
+	init_options = { hostInfo = "neovim" },
+	cmd = { mason_dir .. "typescript-language-server", "--stdio" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+	root_dir = function(bufnr, done_callback)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		done_callback(util.root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git")(fname))
+	end,
 }
--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -1,39 +1,40 @@
-vim.g.mapleader = ' '
-vim.keymap.set('i', '<C-c>', '<Esc>');
-vim.keymap.set('n', '<leader>e', '<CMD>:Oil --float<CR>')
-vim.keymap.set('n', '<Esc>', '<Esc>:noh<CR>', { silent = true })
+vim.g.mapleader = " "
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<leader>e", "<CMD>:Oil --float<CR>")
+vim.keymap.set("n", "<Esc>", "<Esc>:noh<CR>", { silent = true })
+
+-- Persistence
+vim.keymap.set("n", "<leader>sl", function()
+	require("persistence").load({ last = true })
+end)
 
 -- Copilot
 vim.g.copilot_no_tab_map = true
-vim.keymap.set('i', '<C-A>', 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false
+vim.keymap.set("i", "<C-A>", 'copilot#Accept("\\<CR>")', {
+	expr = true,
+	replace_keycodes = false,
 })
-vim.keymap.set('i', '<C-S>', '<Plug>(copilot-accept-line)', {
-})
-vim.keymap.set('i', '<C-D>', '<Plug>(copilot-accept-word)', {
-})
-vim.keymap.set('i', '<C-F>', '<Plug>(copilot-next)', {
-})
+vim.keymap.set("i", "<C-S>", "<Plug>(copilot-accept-line)", {})
+vim.keymap.set("i", "<C-D>", "<Plug>(copilot-accept-word)", {})
+vim.keymap.set("i", "<C-F>", "<Plug>(copilot-next)", {})
 
 -- Open references without focusing on quickfix list
 vim.keymap.set("n", "grr", function()
-  local win = vim.api.nvim_get_current_win()
-  vim.lsp.buf.references(nil, {
-    on_list = function(items, title)
-      vim.fn.setqflist({}, " ", items)
-      vim.cmd.copen()
-      vim.api.nvim_set_current_win(win)
-    end,
-  })
-end
-)
+	local win = vim.api.nvim_get_current_win()
+	vim.lsp.buf.references(nil, {
+		on_list = function(items, title)
+			vim.fn.setqflist({}, " ", items)
+			vim.cmd.copen()
+			vim.api.nvim_set_current_win(win)
+		end,
+	})
+end)
 
 -- Toggle quickfix list
-vim.keymap.set('n', '<leader>q', function()
-  local qf_winid = vim.fn.getqflist({ winid = 0 }).winid
-  local action = qf_winid > 0 and 'cclose' or 'copen'
-  vim.cmd('botright ' .. action)
+vim.keymap.set("n", "<leader>q", function()
+	local qf_winid = vim.fn.getqflist({ winid = 0 }).winid
+	local action = qf_winid > 0 and "cclose" or "copen"
+	vim.cmd("botright " .. action)
 end, noremap_silent)
 
 -- Center
@@ -42,21 +43,32 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-
 -- Harpoon
 local harpoon = require("harpoon")
-vim.keymap.set("n", "<leader>H", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>H", function()
+	harpoon:list():add()
+end)
+vim.keymap.set("n", "<leader>h", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
 
-vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>1", function()
+	harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<leader>2", function()
+	harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<leader>3", function()
+	harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<leader>4", function()
+	harpoon:list():select(4)
+end)
 
 -- Open current file and line in devtools
 vim.keymap.set("n", "<leader>oc", function()
-  local filenameAndLine = vim.fn.expand("%:t") .. ":" .. vim.fn.line(".")
-  local script = [[
+	local filenameAndLine = vim.fn.expand("%:t") .. ":" .. vim.fn.line(".")
+	local script = [[
     tell application "Google Chrome"
       activate
       tell application "System Events"
@@ -68,11 +80,11 @@ vim.keymap.set("n", "<leader>oc", function()
       end tell
     end tell
   ]]
-  script = script:gsub("<<filenameAndLine>>", filenameAndLine)
-  vim.print("Running script: " .. script)
-  vim.system({
-    "osascript",
-    "-e",
-    script,
-  })
-end, { desc = "Open chrome dev tools and run \"open file\" with current file and line" })
+	script = script:gsub("<<filenameAndLine>>", filenameAndLine)
+	vim.print("Running script: " .. script)
+	vim.system({
+		"osascript",
+		"-e",
+		script,
+	})
+end, { desc = 'Open chrome dev tools and run "open file" with current file and line' })
