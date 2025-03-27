e00834b456a1e64a8f160b1ffa7a05c14a03551b
Progressandro <progressandro@gmail.com>
2025-03-26 23:37:04 -0300

    multigrep for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -22,8 +22,9 @@ return {
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fr', ':Telescope frecency workspace=CWD<CR>', { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+    require "config.telescope.multigrep".setup()
   end,
 }
