3a6dd2ef427f738ac3cfff817284666c2072745f
Progressandro <progressandro@gmail.com>
2025-03-26 22:22:14 -0300

    Remove lazydev until it works again

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-  {
     'saghen/blink.cmp',
     dependencies = { 'rafamadriz/friendly-snippets' },
 
@@ -24,16 +13,7 @@ return {
       fuzzy = { implementation = "prefer_rust_with_warning" },
       signature = { enabled = true },
       sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
     },
     opts_extend = { "sources.default" }
