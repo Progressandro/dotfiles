e1477baebf52e0e3ad217fec4307b66c31241a2b
Alejandro F <progressandro@gmail.com>
2025-03-27 22:06:42 -0300

    default typescript-language-server

--- .config/nvim/lsp/typescript.lua
+++ .config/nvim/lsp/typescript.lua
@@ -1,8 +1,6 @@
-local mason_dir = vim.fn.stdpath("data") .. "/mason/bin/"
-local util = require("config.util")
 return {
 	init_options = { hostInfo = "neovim" },
-	cmd = { mason_dir .. "typescript-language-server", "--stdio" },
+	cmd = { "typescript-language-server", "--stdio" },
 	filetypes = {
 		"javascript",
 		"javascriptreact",
@@ -11,8 +9,8 @@ return {
 		"typescriptreact",
 		"typescript.tsx",
 	},
-	root_dir = function(bufnr, done_callback)
-		local fname = vim.api.nvim_buf_get_name(bufnr)
-		done_callback(util.root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git")(fname))
-	end,
+	root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+	preferences = {
+		importModuleSpecifierPreference = "relative",
+	},
 }
